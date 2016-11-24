c89996b929bce875422ca0463ceee297fb9a8d40
Szaymon <szajmon7@wp.pl>
2016-11-24 23:29:50 +0100

    Dzień 2: Pierwsze spotkanie
    Po wejściu do Narnii pierwsze co ujrzałem to przepiękny krajobraz . Nigdy wcześniej nie widziałem takiego cudownego widoku .

---
+++ logowanie.php
@@ -0,0 +1,14 @@
+﻿<?php
+session_start();
+$polacz = mysql_connect('localhost','root','');
+mysql_select_db('serwis',$polacz);
+
+$submit = $_POST['loguj'];
+if($submit)
+{
+    $login = strip_tags($_POST['login']);
+    $haslo = strip_tags($_POST['haslo1']);
+	
+if (mysql_num_rows(mysql_query("SELECT login, haslo FROM uzytkownicy WHERE login= '".$login."' AND haslo = '".($haslo1)."';")) > 0){		
+		
+?>
---
+++ rejestracja.php
@@ -0,0 +1,34 @@
+﻿<?php
+$polacz = mysql_connect('localhost','root','');
+mysql_select_db('serwis',$polacz);
+
+$submit = $_POST['Rejestruj'];
+if($submit)
+{
+    $login = strip_tags($_POST['login']);
+    $haslo = strip_tags($_POST['haslo1']);
+    $powtorz_haslo = strip_tags($_POST['haslo2']);
+    $email = strip_tags($_POST['email']);
+    $imie = strip_tags($_POST['imie']);
+    $nazwisko = strip_tags($_POST['nazwisko']);
+    $telefon = strip_tags($_POST['telefon']);
+    $data_ur = strip_tags($_POST['data_ur']);
+}
+if (mysql_num_rows(mysql_query("SELECT login FROM uzytkownicy WHERE login = '".$login."';")) == 0)
+    {
+        if (mysql_num_rows(mysql_query("SELECT email FROM uzytkownicy WHERE email = '".$email."';")) == 0)
+        {
+            if ($haslo == $powtorz_haslo) 
+            {
+                mysql_query("INSERT INTO uzytkownicy SET login='$login ', haslo='$haslo' , email='$email',imie='$imie',nazwisko='$nazwisko',telefon='$telefon ',data_ur='$data_ur '" ); 
+ 
+                echo "Konto zostało utworzone!";
+            }
+            else echo "Hasła nie są takie same";
+        }
+        else echo "Podany email jest już zajęty.";
+    }
+    else echo "Podany login jest zajęty" ;
+
+?>
+
