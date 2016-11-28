e6ba360c47cd7fd5e195e1db67458e56fcacceea
Szaymon <szajmon7@wp.pl>
2016-11-28 12:17:50 +0100

    Add files via upload

--- logowanie.php
+++ logowanie.php
@@ -1,25 +1,25 @@
 ﻿<?php
-$polacz = mysql_connect('localhost','root','');
-mysql_select_db('serwis',$polacz);
+$polacz = mysql_connect('localhost','root','') or die ("nie można połączyć z bazą danych");
+mysql_select_db('serwis',$polacz) or die("nie ma takiej bazy danych");
 
 $submit = $_POST['loguj'];
 if($submit)
 {
     $login = strip_tags($_POST['login']);
-    $haslo = strip_tags($_POST['haslo1']);
+    $haslo = strip_tags($_POST['haslo']);
 
 if (mysql_num_rows(mysql_query("SELECT login FROM uzytkownicy WHERE login  = '".$login."';")) > 0)
 	{	
-	if (mysql_num_rows(mysql_query("SELECT haslo FROM uzytkownicy WHERE haslo = '".$haslo1."';")) > 0)
-	{
+	if (mysql_num_rows(mysql_query("SELECT haslo FROM uzytkownicy WHERE haslo = '".$haslo."';")) > 0)
+		{
 			session_start();
 				$session_login = $row['login'];
 				$_SESSION['login'] = $session_login;
-				$session_haslo = $row['haslo1'];
-				$_SESSION['haslo1'] = $session_haslo;
-	}
+				$session_haslo = $row['haslo'];
+				$_SESSION['haslo'] = $session_haslo;
+		}
 		else echo "Zły login";
 	}
-		else echo"Chujowe hasło popraw";
+		else echo"Hasło popraw";
 }
 ?>
